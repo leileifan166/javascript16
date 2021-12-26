5f4e95dd729f2ae372f5512d342a19a8025bbb53
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-26 09:43:39 +0900

    Add files via upload

---
+++ javascript16/myjs.js
@@ -0,0 +1,28 @@
+var a;
+var b = 'こんにちはjavascript';
+var b = '零基础学javascript';
+document.write(a);
+document.write("<br>");
+document.write(b);
+document.write("<br>");
+//document.write(c);
+var num = 100;
+num = '有一条路，走过了总会想起';
+document.write(num);
+/* var names = '科比・布莱恩特';
+var alias = '小飞侠';
+var height = 198;
+var score = 33643;
+var achievement = '五届NBA总冠军';
+var position = '得分后卫/小前锋';
+document.write("<h1>科比・布莱恩特是前NBA最著名的篮球运动员之一</h1><br>");
+document.write("<br>");
+document.write(names + "<br>");
+document.write('别名: ' + alias + "<br>");
+document.write('身高: ' + height + "<br>");
+document.write('总得分: ' + score + "<br>");
+document.write('主要成就: ' + achievement + "<br>");
+document.write('场上位置: ' + position + "<br>"); */
+
+
+
